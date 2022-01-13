499e598c8326a946a5fda37c0f4c0ba76e9883f4
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-13 08:21:35 -0500

    Update gitignore

--- src/main/java/com/pragma/config/PragmaConfig.java
+++ src/main/java/com/pragma/config/PragmaConfig.java
@@ -3,9 +3,7 @@ package com.pragma.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Configuration
 @ConfigurationProperties("spring.datasource")
